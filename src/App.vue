82c88606b8597fb349475d2629a153ce407ba814
CaptoshkaLA <captoshkala@gmail.com>
2022-08-22 04:38:38 +0300

    add the main files to launch the application

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Home />
+</template>
+<script lang="ts">
+import LoginForm from '@/modules/login/views/LoginForm.vue';
+import Home from '@/modules/portal/views/Home.vue';
+import { mapGetters } from 'vuex';
+export default {
+  name: 'App',
+  components: {
+    Home,
+    LoginForm,
+  },
+  computed: {
+    ...mapGetters(['getUser']),
+  },
+};
+</script>
+<style lang="scss">
+@import '@/common/scss/style.scss';
+#app {
+  font-family: 'Roboto Slab', 'Bebas Neue', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
